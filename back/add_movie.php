f2923d73a321a95163035199245f2d3b3a78102b
oliviawu <oliviawu0301@gmail.com>
2025-01-09 16:08:52 +0800

    建立新增院線片及編輯院線片表單

--- back/add_movie.php
+++ back/add_movie.php
@@ -5,19 +5,19 @@
     display: flex;
 }
 
-.form>div {
+.form div:nth-child(1) {
     width: 15%;
     padding: 5px;
     text-align-last: justify;
 }
 
-.form>div>div {
+.form div:nth-child(2) {
     width: 85%;
     padding: 5px;
 }
 </style>
+<form action="./api/add_movie.php" method="post" enctype="multpart/form-data">
 
-<form action="./api/add_movie.php" method="post" enctype="multipart/form-data">
     <div style="width:70%;margin:auto;display:flex;">
         <div style="width:15%">影片資料</div>
         <div style="width:85%">
@@ -28,7 +28,7 @@
             <div class="form">
                 <div>分級</div>：
                 <div>
-                    <select name="" id="">
+                    <select name="level" id="">
                         <option value="1">普通級</option>
                         <option value="2">保護級</option>
                         <option value="3">輔導級</option>
@@ -37,7 +37,13 @@
                 </div>
             </div>
             <div class="form">
-                <div>上映日期：</div>：
+                <div>片長</div>：
+                <div>
+                    <input type="number" name="length" id="">
+                </div>
+            </div>
+            <div class="form">
+                <div>上映日期</div>：
                 <div>
                     <select name="year" id="">
                         <option value="2025">2025</option>
@@ -45,41 +51,41 @@
                     </select>年
                     <select name="month" id="">
                         <?php
-                        for($i=i;$i<=12;$i++){
-                        echo"<option value='$i'>$i</option>";
-                        }
+                    for($i=1;$i<=12;$i++){
+                        echo "<option value='$i'>$i</option>";
+                    }
                     ?>
                     </select>月
                     <select name="day" id="">
                         <?php
-                        for($i=i;$i<=31;$i++){
-                        echo"<option value='$i'>$i</option>";
-                        }
+                    for($i=1;$i<=31;$i++){
+                        echo "<option value='$i'>$i</option>";
+                    }
                     ?>
                     </select>日
                 </div>
             </div>
             <div class="form">
-                <div>發行商：</div>
+                <div>發行商</div>：
                 <div><input type="text" name="publish" id=""></div>
             </div>
             <div class="form">
-                <div>導演：</div>
+                <div>導演</div>：
                 <div><input type="text" name="director" id=""></div>
             </div>
             <div class="form">
-                <div>預告影片</div>
+                <div>預告影片</div>：
                 <div><input type="file" name="trailer" id=""></div>
             </div>
             <div class="form">
-                <div>電影海報</div>
+                <div>電影海報</div>：
                 <div><input type="file" name="poster" id=""></div>
             </div>
         </div>
     </div>
     <div style="display:flex;width:70%;margin:auto;">
         <div style="width:15%;">劇情簡介</div>
-        <div style="width:85%;">
+        <div style="width:85%">
             <textarea name="intro" id="" style="width:99%"></textarea>
         </div>
     </div>
