8e5edd88c3bb4eea1fdb7471ac07721e88b36ead
oliviawu <oliviawu0301@gmail.com>
2025-01-10 11:04:20 +0800

    完成編輯電影功能

--- api/save_movie.php
+++ api/save_movie.php
@@ -5,16 +5,19 @@ if(!empty($_FILES['trailer']['tmp_name'])){
     $_POST['trailer']=$_FILES['trailer']['name'];
 }
 
-if(!empty($_FILES['trailer']['tmp_name'])){
+if(!empty($_FILES['poster']['tmp_name'])){
+    
     move_uploaded_file($_FILES['poster']['tmp_name'],"../upload/{$_FILES['poster']['name']}");
     $_POST['poster']=$_FILES['poster']['name'];
 }
 
-$_POST['ondate']=$_POST['year'] . "-" .$_POST['month'] . "-" .$_POST['day'] ;
-unset($_POST['year'],$_POST['month'],$_POST['day'] );
+$_POST['ondate']=$_POST['year'] ."-" .$_POST['month'] ."-" .$_POST['day'];
+unset($_POST['year'],$_POST['month'],$_POST['day']);
 
-$_POST['sh']=1;
-$_POST['rank']=$Movie->max('id')+1;
+if(!isset($_POST['id'])){
+    $_POST['sh']=1;
+    $_POST['rank']=$Movie->max('id')+1;
+}
 
 $Movie->save($_POST);
 
