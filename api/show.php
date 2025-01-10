0237083c3b5b794b7b8773f5b1a217bfb01d764e
oliviawu <oliviawu0301@gmail.com>
2025-01-10 09:06:59 +0800

    完成院線片後台的顯示及排序功能

---
+++ api/show.php
@@ -0,0 +1,15 @@
+<?php include_once "db.php";
+
+// $row=$Movie->find($_POST['id']);
+// if($row['sh']==1){
+//     $row['sh']=0;
+// }else{
+
+//     $row['sh']==1;
+// }
+
+$row=$Movie->find($_POST['id']);
+
+$row['sh']=($row['sh']+1)%2;
+
+$Movie->save($row);
